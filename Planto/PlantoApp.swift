dd3b159bb80dba5fd9f363f74e12694d755553e3
WAAD ALSAIF <50026099+WaadAlsaif@users.noreply.github.com>
2022-08-30 08:53:36 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Waad Alsaif on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
